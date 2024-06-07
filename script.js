328c61bc29b532127adbeaf073de53978d765038
Deelfc <84929683+Deelfc@users.noreply.github.com>
2024-06-07 06:21:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+// app.js
+document.addEventListener("DOMContentLoaded", () => {
+    const modal = document.getElementById("myModal");
+    const openModalBtn = document.getElementById("openModalBtn");
+    const closeBtn = document.querySelector(".close");
+
+    openModalBtn.onclick = function() {
+        modal.style.display = "flex";
+    }
+
+    closeBtn.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
+});
